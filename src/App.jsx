12e11e938c6ae52274cbf0715f4aaaeea3efe51f
alvaro <alvaro.herranz@mataro.epiaedu.cat>
2024-02-03 18:40:46 +0100

    complete exercices

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header } from "./Header";
+import { Total } from "./Total";
+import { Content } from "./Content";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
-  const name = "Alvaro";
   return (
-    <>
-     <h1>Hello {`${name}`}</h1>
-    </>
-  )
-}
+    <div>
+      <Header course={course}></Header>
+      <Content 
+        part1={part1} 
+        part2={part2} 
+        part3={part3} 
+        ex1={exercises1}
+        ex2={exercises2}
+        ex3={exercises3}
+        ></Content>
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      ></Total>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Total.jsx
@@ -0,0 +1,5 @@
+export const Total = ({exercises1, exercises2, exercises3}) => {
+    return( 
+    <p>Number of exercices {exercises1 + exercises2 + exercises3}</p>
+    );
+};
